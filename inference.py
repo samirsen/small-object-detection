ef53e2384330bd6dac6b50bf2159d99eaca3cbd2
Samir Sen <samirsen@stanford.edu>
2019-06-05 07:56:49 -0700

    updating upscale-factor

--- inference.py
+++ inference.py
@@ -18,7 +18,7 @@ from utils import array_tool as at
 from SRGAN import Generator
 
 parser = argparse.ArgumentParser(description='Test Single Image')
-parser.add_argument('--upscale_factor', default=1, type=int, help='super resolution upscale factor')
+parser.add_argument('--upscale_factor', default=4, type=int, help='super resolution upscale factor')
 parser.add_argument('--test_mode', default='GPU', type=str, choices=['GPU', 'CPU'], help='using GPU or CPU')
 parser.add_argument('--model_name', default='netG_epoch_4_100.pth', type=str, help='generator model epoch name')
 opt = parser.parse_args()
@@ -38,11 +38,11 @@ if TEST_MODE:
     gan_model.cuda()
     trainer.cuda()
     gan_model.load_state_dict(torch.load('epochs/' + MODEL_NAME))
-    trainer.load('epochs/samir_fast_rcnn_epoch60.pth')
+    # trainer.load('epochs/samir_fast_rcnn_epoch60.pth')
 
 else:
     gan_model.load_state_dict(torch.load('epochs/' + MODEL_NAME, map_location=lambda storage, loc: storage))
-    trainer.load('epochs/samir_fast_rcnn_epoch60.pth')
+    # trainer.load('epochs/samir_fast_rcnn_epoch60.pth')
 
 image = read_image('misc/demo.jpg')
 image = Variable(ToTensor()(image), volatile=True).unsqueeze(0)
@@ -54,13 +54,13 @@ out = gan_model(image)
 out_img = ToPILImage()(out[0].data.cpu())
 out_img.save('out_srf_' + str(UPSCALE_FACTOR) + '_' + IMAGE_NAME)
 
-_bboxes, _labels, _scores = trainer.faster_rcnn.predict(out_img, visualize=True)
-ax = vis_bbox(at.tonumpy(img[0]),
-         at.tonumpy(_bboxes[0]),
-         at.tonumpy(_labels[0]).reshape(-1),
-         at.tonumpy(_scores[0]).reshape(-1))
-
-plt.show()
+# _bboxes, _labels, _scores = trainer.faster_rcnn.predict(out_img, visualize=True)
+# ax = vis_bbox(at.tonumpy(img[0]),
+#          at.tonumpy(_bboxes[0]),
+#          at.tonumpy(_labels[0]).reshape(-1),
+#          at.tonumpy(_scores[0]).reshape(-1))
+#
+# plt.show()
 
 elapsed = (time.clock() - start)
 print('cost' + str(elapsed) + 's')
